7dc1210dc8ed74c460e11eaac0cc4c78bb3c5795
paketchino <chansgorn@gmail.com>
2022-08-07 00:39:09 +0300

    0. ToOne [#6873 #329099]

--- src/main/java/ru/job4j_todo/Job4jTodoApplication.java
+++ src/main/java/ru/job4j_todo/Job4jTodoApplication.java
@@ -1,5 +1,6 @@
 package ru.job4j_todo;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -7,6 +8,11 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Properties;
 
 @SpringBootApplication
 public class Job4jTodoApplication {
@@ -16,6 +22,41 @@ public class Job4jTodoApplication {
 		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
 				.configure().build();
 		return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+	}
+
+
+	private Properties loadDbProperties() {
+		Properties cfg = new Properties();
+		try (BufferedReader io = new BufferedReader(
+				new InputStreamReader(
+						Objects.requireNonNull(Job4jTodoApplication.class.getClassLoader()
+								.getResourceAsStream("db.properties"))
+				)
+		)) {
+			cfg.load(io);
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+		try {
+			Class.forName(cfg.getProperty("jdbc.driver"));
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+		return cfg;
+	}
+
+	@Bean
+	public BasicDataSource loadPool() {
+		Properties cfg = loadDbProperties();
+		BasicDataSource pool = new BasicDataSource();
+		pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
+		pool.setUrl(cfg.getProperty("jdbc.url"));
+		pool.setUsername(cfg.getProperty("jdbc.username"));
+		pool.setPassword(cfg.getProperty("jdbc.password"));
+		pool.setMinIdle(5);
+		pool.setMaxIdle(10);
+		pool.setMaxOpenPreparedStatements(100);
+		return pool;
 	}
 
 	public static void main(String[] args) {
